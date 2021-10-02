ce37bdb62c3ff659ceee3488331cc7abd213bbfd
wanglekai <xiaoguailele@foxmail.com>
2021-10-02 15:07:54 +0800

    优化：合并webpack配置

--- webpack.client.js
+++ webpack.client.js
@@ -1,24 +1,12 @@
 const path = require('path')
+const baseConfig = require('./webpack.base')
+const merge = require('webpack-merge')
 
-module.exports = {
-    mode: 'development',
+const config = {
     entry: './src/client/index.js',
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
-            }
-        ]
     }
 }
+module.exports = merge(baseConfig, config)
--- webpack.server.js
+++ webpack.server.js
@@ -1,25 +1,13 @@
 const path = require('path')
+const baseConfig = require('./webpack.base')
+const merge = require('webpack-merge')
 
-module.exports = {
-    mode: 'development',
+const config = {
     target: 'node',
     entry: './src/server/index.js',
     output: {
         path: path.join(__dirname, 'build'),
         filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
-            }
-        ]
     }
 }
+module.exports = merge(baseConfig, config)
